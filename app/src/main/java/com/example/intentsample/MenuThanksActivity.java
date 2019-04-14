31d3191e1dc70f89356bc59d4aad8aeb99b33834
hojihoji <yuya.m.tokai@gmail.com>
2019-04-14 19:34:34 +0900

    2nd

--- app/src/main/java/com/example/intentsample/MenuThanksActivity.java
+++ app/src/main/java/com/example/intentsample/MenuThanksActivity.java
@@ -1,7 +1,10 @@
 package com.example.intentsample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MenuThanksActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MenuThanksActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_thanks);
+
+        Intent intent = getIntent();
+        String menuName = intent.getStringExtra("menuName");
+        String menuPrice = intent.getStringExtra("menuPrice");
+
+        TextView tvMenuName = findViewById(R.id.tvMenuName);
+        TextView tvMenuPrice = findViewById(R.id.tvMenuPrice);
+
+        tvMenuName.setText(menuName);
+        tvMenuPrice.setText(menuPrice);
     }
+
+    public void onBackButtonClick(View view){
+        finish();
+    }
+
 }
